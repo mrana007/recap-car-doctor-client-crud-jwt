681c0983093129c0b2b3d6ae977793a75de2675c
mrana007 <rana.es.bc@gmail.com>
2023-10-31 01:45:56 +0600

    set loggeduser

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -32,16 +32,27 @@ const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = {email: userEmail};
       setUser(currentUser);
       console.log('current user', currentUser);
       setLoading(false);
       // if user exists then issue a token
       if(currentUser){
-        const loggedUser = {email: currentUser.email};
+        
         axios.post('http://localhost:5000/jwt',loggedUser, {withCredentials: true})
         .then(res =>{
           console.log('token response',res.data);
+        })
+      }
+      else{
+        axios.post('http://localhost:5000/logout', loggedUser, {
+          withCredentials: true
+        })
+        .then(res =>{
+          console.log(res.data);
         })
       }
     });
